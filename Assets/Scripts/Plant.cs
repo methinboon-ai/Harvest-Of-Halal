b5a8562fedae8928ecb559022c5c7849b81175af
methinboon-ai <methin.boon@bumail.net>
2025-11-11 13:59:32 +0700

    test

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Plant : MonoBehaviour
+{
+    [SerializeField] List<GameObject> PlantPrefabs = new List<GameObject>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
