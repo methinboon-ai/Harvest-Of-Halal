9e684113969dfa4fb3fdc1da7e7c42901954b386
methinboon-ai <methin.boon@bumail.net>
2025-11-11 13:11:23 +0700

    map fix 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check if the left mouse button is clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Create a ray from the camera, through the mouse position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // Perform the raycast
+            if (Physics.Raycast(ray, out hit))
+            {
+                // If the ray hits an object, get its Transform
+                Transform targetTransform = hit.transform;
+                Debug.Log("Mouse clicked on object: " + targetTransform.name);
+
+                // You can now use targetTransform for various actions,
+                // such as moving towards it, interacting with it, etc.
+            }
+            else
+            {
+                Debug.Log("Mouse clicked on empty space.");
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerUI : MonoBehaviour
 {
-    int garden;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
