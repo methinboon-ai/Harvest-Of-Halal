4cb7ab50393c217dcbfb206da54450cfec43bf45
methinboon-ai <methin.boon@bumail.net>
2025-11-11 13:48:20 +0700

    TargetMouse System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,57 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    
+    [SerializeField] Canvas PlayerUI;
+    TMP_Text InteractText;
+    TMP_Text SelectText;
 
     void Start()
     {
-        
+        InteractText = PlayerUI.transform.Find("InteractText").GetComponent<TMP_Text>();
+        SelectText = PlayerUI.transform.Find("SelectText").GetComponent<TMP_Text>();
+        InteractText.gameObject.SetActive(false);
+    }
+
+    Transform FindMouseTarget()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Perform the raycast
+        if (Physics.Raycast(ray, out hit))
+        {
+            // If the ray hits an object, get its Transform
+            Transform targetTransform = hit.transform;
+
+            if (targetTransform.name == "Planter")
+            {
+                return targetTransform;
+            }
+            return null;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    void DisplayMouseTarget()
+    {
+        Transform target = FindMouseTarget();
+        if (target != null) {
+            InteractText.gameObject.SetActive(true);
+            if (target.name == "Planter")
+            {
+                SelectText.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            InteractText.gameObject.SetActive(false);
+        }
     }
 
     void Update()
@@ -14,24 +59,9 @@ public class Player : MonoBehaviour
         // Check if the left mouse button is clicked
         if (Input.GetMouseButtonDown(0))
         {
-            // Create a ray from the camera, through the mouse position
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            // Perform the raycast
-            if (Physics.Raycast(ray, out hit))
-            {
-                // If the ray hits an object, get its Transform
-                Transform targetTransform = hit.transform;
-                Debug.Log("Mouse clicked on object: " + targetTransform.name);
-
-                // You can now use targetTransform for various actions,
-                // such as moving towards it, interacting with it, etc.
-            }
-            else
-            {
-                Debug.Log("Mouse clicked on empty space.");
-            }
+            Transform target = FindMouseTarget();
+            Debug.Log(target);
         }
+        DisplayMouseTarget();
     }
 }
